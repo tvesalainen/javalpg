756c141c80139629ec9c94e57527bf7813d0d2ba
Timo Vesalainen <timo.vesalainen@iki.fi>
2015-06-29 14:58:32 +0100

    Add WildcardMatcher

---
+++ src/test/java/org/vesalainen/regex/WildcardMatcherTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 tkv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.vesalainen.regex;
+
+import org.junit.Test;
+import org.vesalainen.regex.WildcardMatcher;
+import static org.junit.Assert.*;
+import org.vesalainen.util.Matcher;
+import org.vesalainen.util.Matcher.Status;
+
+/**
+ *
+ * @author tkv
+ */
+public class WildcardMatcherTest
+{
+    
+    public WildcardMatcherTest()
+    {
+    }
+
+    @Test
+    public void test1()
+    {
+        WildcardMatcher<String> wm = new WildcardMatcher<>();
+        wm.addExpression("$??RMC", "rmc");
+        wm.addExpression("$??GLL", "gll");
+        wm.addExpression("$HCHDT", "hdt");
+        wm.compile();
+        assertEquals(Status.Ok, wm.match('$'));
+        assertEquals(Status.Ok, wm.match('G'));
+        assertEquals(Status.Ok, wm.match('P'));
+        assertEquals(Status.Ok, wm.match('G'));
+        assertEquals(Status.Ok, wm.match('L'));
+        assertEquals(Status.Match, wm.match('L'));
+    }
+    
+}
